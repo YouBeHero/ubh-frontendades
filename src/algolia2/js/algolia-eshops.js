6be274561d182b5240a479b53760b85e41421857
murtazahassani <murtazahassani105@gmail.com>
2020-11-19 18:07:30 +0200

    So loading has been added

--- src/algolia2/js/algolia-eshops.js
+++ src/algolia2/js/algolia-eshops.js
@@ -712,13 +712,3 @@ search.addWidget(
 search.start();
 
 
-
-// loading section
- $(window).on('load', function() {
-  if ($('#preloader').length) {
-    $('#preloader').delay(100).fadeOut('slow', function() {
-      $(this).remove();
-    });
-  }
-});
-
