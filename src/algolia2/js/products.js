8896f9d94f8369435e37007231a49782546d830a
bbouba <bboubcar@gmail.com>
2021-01-21 16:37:05 +0200

    Last setUp for json data updating

--- src/algolia2/js/products.js
+++ src/algolia2/js/products.js
@@ -3,32 +3,34 @@
 
 const algoliaSearch = require("algoliasearch");
 const client = algoliaSearch("3CUMU7CU6Q", "9b7885176dc700e465237047fce8057a"); //(AppID, AdminAPIKey)
-const index = client.initIndex("ybh_index");
+const index = client.initIndex("ybhIndex");
 
 
 //  Local Json file.
-const products = (require("../_JSONs/products-shoes.json"));
-
+const products = (require("../_JSONs/products-clothes.json"));
+// console.log(products.length)
 //  Before to send the json files on Algolia, i convert the full price and price string-->number
 const produc = products.filter(element => {
     element.full_price = parseInt(element.full_price);
     element.price = parseInt(element.price);
     element.price = parseInt(element.price);
 
-    //  There is some products wich don't have size key
+//     //  There is some products wich don't have size key
     if(element.size){
-        //  Some sizes are separeted by '/' and another by ',' 
+//         //  Some sizes are separeted by '/' and another by ',' 
         if (element.size.includes('/')){
             element.size = element.size.split('/')
         }else{
             element.size = element.size.split(',')
         }
-        element.size = element.size.map(item => parseInt(item))
+
         return element
+        
     }   
 });
+
 //  Then push the data on Algolia
 index.replaceAllObjects(produc, { autoGenerateObjectIDIfNotExist: true }).then(({ objectIDs }) => {
     console.log(objectIDs);
 })
-.catch(err => console.error(err));
+.catch(err => console.error(err))
