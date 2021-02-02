8f014c2f42c3eb826e2ca07e2ba6b00b1c00faff
icon32 <dionmpolanhs@gmail.com>
2021-02-02 20:40:29 +0200

    Created: Getting the right url from YouBeHero . Dionisis Setup: the sessions . Dionisis Setup: small banner after the contineu to checkout button  . Dionisis Setup: Frontpage display if the user comes from youbehero . Dionisis Setup: Main Backend Page  .Bill

--- src/WP-Plugin/includes/admin-pages/admin-page.php
+++ src/WP-Plugin/includes/admin-pages/admin-page.php
@@ -1,19 +1,19 @@
-
-<?php
-
-function main_admin_menu() {
-    
-  //add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null )
-    add_menu_page(__( 'YouBeHero', 'you-be-hero' ), __( 'YouBeHero', 'you-be-hero' ),'manage_options',  'you-be-hero', 'main_admin_page_contents','dashicons-schedule', 1);
-}
-
-add_action( 'admin_menu', 'main_admin_menu' );
-
-
-
-//Main Admin Page
-include "main-page.php";
-
-
-
-
+
+<?php
+
+function main_admin_menu() {
+    
+  //add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null )
+    add_menu_page(__( 'YouBeHero', 'you-be-hero' ), __( 'YouBeHero', 'you-be-hero' ),'manage_options',  'you-be-hero', 'main_admin_page_contents','dashicons-schedule', 1);
+}
+
+add_action( 'admin_menu', 'main_admin_menu' );
+
+
+
+//Main Admin Page
+include "main-page.php";
+
+
+
+
--- src/WP-Plugin/js/main.js
+++ src/WP-Plugin/js/main.js
@@ -1,46 +1,46 @@
-//
-//  *=====================================
-//          Footer JS File
-//  *=====================================
-//
-
-jQuery(document).ready(function($) {
-
-//Get Parammeters from url Start
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-};
-
-if (getUrlParameter('ybh-user-id')) {
-    var tech = getUrlParameter('ybh-user-id');
-    //alert(tech)
-    window.localStorage
-    localStorage.setItem("user-id", tech);
-
-    $('#urlcal').html(tech);
-} else {
-    $('#urlcal').html('No Parameter was passed');
-}
-
-//Get Parammeters from url End
-
-let userId = localStorage.getItem("user-id")
-
-$('#ybh-userid').html('Your User id is: ' + userId);
-
-$('#localstrid').html(userId);
-
-
-})
-
+//
+//  *=====================================
+//          Footer JS File
+//  *=====================================
+//
+
+jQuery(document).ready(function($) {
+
+//Get Parammeters from url Start
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+if (getUrlParameter('ybh-user-id')) {
+    var tech = getUrlParameter('ybh-user-id');
+    //alert(tech)
+    window.localStorage
+    localStorage.setItem("user-id", tech);
+
+    $('#urlcal').html(tech);
+} else {
+    $('#urlcal').html('No Parameter was passed');
+}
+
+//Get Parammeters from url End
+
+let userId = localStorage.getItem("user-id")
+
+$('#ybh-userid').html('Your User id is: ' + userId);
+
+$('#localstrid').html(userId);
+
+
+})
+
