04019236d2318abdf2387e849fc363c42e99bac1
kostikas2002 <papandrk@gmail.com>
2021-03-22 01:39:18 +0200

    v2 of client-side referral tracking script (posts to backend)

--- src/ubh-api/ybh-client.js
+++ src/ubh-api/ybh-client.js
@@ -49,7 +49,7 @@ function ybhScript() {
 	function landingPageScript() {
 	
 		const queryString= new URLSearchParams( window.location.search ) ;
-		const keys = [ "youbehero-user-id" , "youbehero-user-name" , "youbehero-cause-id" , "youbehero-cause-name" , "youbehero-cause-logo-url" ] ;
+		const keys = [ "ubhTr" , "commission_percentage" , "cause_name" , "cause_logo_url" ] ;
 		let values = [] ;
 		
 		if ( hasArrivedFromYbh() ) {
@@ -91,25 +91,27 @@ function ybhScript() {
 			// const ybhBackgroundUrl = "https://www.youbehero.com/img/background-images/coolgradient.svg" ;
 			const ybhBackgroundUrl = "https://www.youbehero.com/img/background-images/video-bg.svg" ;
 			
-			const ybhUser = JSON.parse( localStorage.getItem( "ybh-referral" ) ) ;
-			const ybhCommission = 0.04 ; // must be custom-set on each site
+			const ybhReferral = JSON.parse( localStorage.getItem( "ybh-referral" ) ) ;
 			const ybhCard = document.createElement( "div" ) ;
 			const totalPriceElement = document.querySelector( ".total-price" ) ; // must be custom-set on each site
 			const proceedToCheckoutElement = document.querySelector( ".proceed-to-checkout" ) ; // must be custom-set on each site
-			ybhCard.style.background = "url(\"" + ybhBackgroundUrl + "\") no-repeat center" ;
+			ybhCard.style.background = "white url(\"" + ybhBackgroundUrl + "\") no-repeat center" ;
 			ybhCard.style.backgroundSize = "cover" ;
 			ybhCard.style.padding = "20px 40px" ;
 			ybhCard.style.margin = "20px 40px" ;
 			// ybhCard.style.border = "2px lightsteelblue solid" ;
 			ybhCard.style.borderRadius = "8px" ;
-			ybhCard.style.boxShadow = "0px 0px 20px -10px #777777, 0px 10px 6px -6px #cccccc " ;
-			ybhCard.innerHTML = `<img src="${ ybhUser[ "youbehero-cause-logo-url" ] }" style="width:200px;"/><br />Hey, ${ ybhUser[ "youbehero-user-name" ] }! By making this purchase, ${ parseInt( totalPriceElement.textContent ) * ybhCommission }€ will be donated to ${ ybhUser[ "youbehero-cause-name" ] }!<br /><img src="${ ybhLogo.src }" style="width:150px;"/>`;
+			ybhCard.style.boxShadow = "0px 0px 20px -10px #cccccccc, 0px 10px 6px -6px #cccccccc" ;
+			ybhCard.innerHTML = 
+				`<div style="display:flex;flex-wrap:wrap;justify-content:space-around;"><div><img src="https://www.youbehero.com${ ybhReferral[ "cause_logo_url" ] }" style="width:200px;"/></div>` + 
+				`<div style="display:flex;flex-direction:column;"><div style="margin-top:1rem;max-width:30rem;">Χάρη σε αυτή την αγορά θα προσφέρεις <strong>${ parseFloat( totalPriceElement.textContent ) * 0.01 * ybhReferral[ "commission_percentage" ] }€</strong> προς την/τον <strong>${ ybhReferral[ "cause_name" ] }</strong> χωρίς κανένα κόστος!</div>` + 
+				`<div style="font-size:0.8rem;margin-top:1rem;">Υποστηρίζεται από το <img src="${ ybhLogo.src }" style="width:75px;position:relative;top:3px;"/></div></div>`;
 			
 			// placed after the total price element
-			totalPriceElement.after( ybhCard ) ;
+			totalPriceElement.parentElement.after( ybhCard ) ; // must be custom-set on each site
 			
 			// placed before the checkout button
-			proceedToCheckoutElement.before( ybhCard.cloneNode( true ) ) ;
+			proceedToCheckoutElement.parentElement.before( ybhCard.cloneNode( true ) ) ; // must be custom-set on each site
 		}
 	}
 	
@@ -125,49 +127,59 @@ function ybhScript() {
 			// const ybhBackgroundUrl = "https://www.youbehero.com/img/background-images/coolgradient.svg" ;
 			const ybhBackgroundUrl = "https://www.youbehero.com/img/background-images/video-bg.svg" ;
 			
-			const ybhUser = JSON.parse( localStorage.getItem( "ybh-referral" ) ) ;
-			const ybhCommission = 0.04 ; // must be custom-set on each site
+			const ybhReferral = JSON.parse( localStorage.getItem( "ybh-referral" ) ) ;
 			const ybhCard = document.createElement( "div" ) ;
 			const totalPriceElement = document.querySelector( ".total-price" ) ; // must be custom-set on each site
 			const finalizePurchaseElement = document.querySelector( ".finalize-purchase" ) ; // must be custom-set on each site
-			ybhCard.style.background = "url(\"" + ybhBackgroundUrl + "\") no-repeat center" ;
+			ybhCard.style.background = "white url(\"" + ybhBackgroundUrl + "\") no-repeat center" ;
 			ybhCard.style.backgroundSize = "cover" ;
 			ybhCard.style.padding = "20px 40px" ;
 			ybhCard.style.margin = "20px 40px" ;
 			// ybhCard.style.border = "2px lightsteelblue solid" ;
 			ybhCard.style.borderRadius = "8px" ;
-			ybhCard.style.boxShadow = "0px 0px 20px -10px #777777, 0px 10px 6px -6px #cccccc " ;
-			ybhCard.innerHTML = `<img src="${ ybhUser[ "youbehero-cause-logo-url" ] }" style="width:200px;"/><br />Hey, ${ ybhUser[ "youbehero-user-name" ] }! By making this purchase, ${ parseInt( totalPriceElement.textContent ) * ybhCommission }€ will be donated to ${ ybhUser[ "youbehero-cause-name" ] }!<br /><img src="${ ybhLogo.src }" style="width:150px;"/>`;
+			ybhCard.style.boxShadow = "0px 0px 20px -10px #cccccccc, 0px 10px 6px -6px #cccccccc" ;
+			ybhCard.innerHTML = 
+				`<div style="display:flex;flex-wrap:wrap;justify-content:space-around;"><div><img src="https://www.youbehero.com${ ybhReferral[ "cause_logo_url" ] }" style="width:200px;"/></div>` + 
+				`<div style="display:flex;flex-direction:column;"><div style="margin-top:1rem;max-width:30rem;">Χάρη σε αυτή την αγορά θα προσφέρεις <strong>${ parseFloat( totalPriceElement.textContent ) * 0.01 * ybhReferral[ "commission_percentage" ] }€</strong> προς την/τον <strong>${ ybhReferral[ "cause_name" ] }</strong> χωρίς κανένα κόστος!</div>` + 
+				`<div style="font-size:0.8rem;margin-top:1rem;">Υποστηρίζεται από το <img src="${ ybhLogo.src }" style="width:75px;position:relative;top:3px;"/></div></div>`;
 			
 			// placed after the total price element
-			totalPriceElement.after( ybhCard ) ;
+			totalPriceElement.parentElement.after( ybhCard ) ;
 			
 			// placed before the finalize button
-			finalizePurchaseElement.before( ybhCard.cloneNode( true ) ) ;
+			finalizePurchaseElement.parentElement.before( ybhCard.cloneNode( true ) ) ;
+			
+			// save totalPrice to localStorage
+			ybhReferral[ "sale_amount" ] = parseFloat( totalPriceElement.textContent ) ;
+			localStorage.setItem( "ybh-referral" , JSON.stringify( ybhReferral ) ) ; 
+			
+			// temporary hard-coded transaction_id
+			ybhReferral[ "transaction_id" ] = "1" ;
+			localStorage.setItem( "ybh-referral" , JSON.stringify( ybhReferral ) ) ; 
 		}
 	}
 	
 	function completedPageScript() {
 		
 		if ( localStorage.getItem( "ybh-referral" ) ) {
-			setupPixel() ;
+			registerTransaction() ;
 			localStorage.removeItem( "ybh-referral" ) ;
 		}
 		
-		function setupPixel() {
-			let src = "http://192.168.1.7:3000/img/ybh-pxl.gif?" ; // lan express server for testing
-			ybhReferral = JSON.parse( localStorage.getItem( "ybh-referral" ) ) ;
-			for ( key in ybhReferral ) {
-				src += key + "=" + encodeURIComponent( ybhReferral[ key ].trim() ) + "&" ;
-			} ;
-			src = src.slice( 0 , -1 ) ;
-			console.log(src)
-			// document.createElement( "img" ).src = src ;
-			const pixel = document.createElement( "img" ) ;
-			pixel.src = src ;
-			pixel.style.width = "0px" ;
-			pixel.style.height = "0px" ;
-			document.querySelector( "div" ).appendChild ( pixel ) ;
+		function registerTransaction() {
+			// let src = "http://192.168.1.7:3000/img/ybh-pxl.gif?" ; // lan express server for testing
+			let src = "https://youbehero.com/gr/test/trackTransaction?" ;
+			const ybhReferral = JSON.parse( localStorage.getItem( "ybh-referral" ) ) ;
+			src += "ubhTr=" + encodeURIComponent( ybhReferral[ "ubhTr" ].trim() ) + "&" +
+				// "sale_amount=" + encodeURIComponent( ybhReferral[ "sale_amount" ].trim() ) + "&" +
+				"sale_amount=" + encodeURIComponent( ybhReferral[ "sale_amount" ] ) + "&" +
+				"transaction_id=" + encodeURIComponent( ybhReferral[ "transaction_id" ].trim() ) ;
+			console.log(src) ;
+			const ybhPixel = document.createElement( "img" ) ;
+			ybhPixel.src = src ;
+			ybhPixel.style.width = "0px" ;
+			ybhPixel.style.height = "0px" ;
+			document.querySelector( "body" ).appendChild ( ybhPixel ) ;
 		}
 	}
 }
